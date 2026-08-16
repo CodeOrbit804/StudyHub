44d4b40d4ef83b6443ce6b3ace9e3a2b3adc7b11
CodeOrbit804 <divyanshuyadavji0001@gmail.com>
2026-08-16 20:08:05 +0530

    Update 10th-sp.js

--- 10th-sp.js
+++ 10th-sp.js
@@ -3,11 +3,11 @@ const sp10 = {
     "Maths": [
         {
             set: "Set-1",
-            pdf: "../sample paper/StudyHubClass10Maths_Set1.pdf"
+            pdf: "StudyHubClass10Maths_Set1.pdf"
         },
         {
             set: "Set-2",
-            pdf: "../sample paper/StudyHubClass10Maths_Set2.pdf"
+            pdf: "StudyHubClass10Maths_Set2.pdf"
         },
         
     ],
@@ -15,22 +15,22 @@ const sp10 = {
     "English": [
         {
             set: "Set-1",
-            pdf: "../sample paper/StudyHubClass10English_Set1.pdf"
+            pdf: "StudyHubClass10English_Set1.pdf"
         },
         {
             set: "Set-2",
-            pdf: "../sample paper/StudyHubClass10English_Set2.pdf"
+            pdf: "StudyHubClass10English_Set2.pdf"
         }
     ],
 
     "Science": [
         {
             set: "Set-1",
-            pdf: " ../sample paper/StudyHubClass10Science_Set1.pdf"
+            pdf: "StudyHubClass10Science_Set1.pdf"
         },
         {
             set: "Set-2",
-            pdf: " ../sample paper/StudyHubClass10Science_Set2.pdf"
+            pdf: "StudyHubClass10Science_Set2.pdf"
         }
     ],
 
@@ -38,11 +38,11 @@ const sp10 = {
     "Social Science": [
         {
             set: "Set-1",
-            pdf: "../sample paper/StudyHub_Class10_MPBoard_SocialScience_Set1_FULL.pdf"
+            pdf: "StudyHub_Class10_MPBoard_SocialScience_Set1_FULL.pdf"
         },
         {
             set: "Set-2",
-            pdf: "../sample paper/StudyHub_Class10_MPBoard_SocialScience_Set2_FULL.pdf"
+            pdf: "StudyHub_Class10_MPBoard_SocialScience_Set2_FULL.pdf"
         }
     ]
 
